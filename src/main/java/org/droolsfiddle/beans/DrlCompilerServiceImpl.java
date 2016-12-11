36b344f312beb9503b80a3e0a82fd6384dbbed52
jvipret <julien.vipret@gmail.com>
2016-12-11 18:47:45 +0100

    apply Apache LICENSE

--- src/main/java/org/droolsfiddle/beans/DrlCompilerServiceImpl.java
+++ src/main/java/org/droolsfiddle/beans/DrlCompilerServiceImpl.java
@@ -1,12 +1,25 @@
+/*   Copyright 2016 Drools Fiddle
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
 package org.droolsfiddle.beans;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.deser.std.StringArrayDeserializer;
 import org.droolsfiddle.persistence.beans.KieBaseWrapper;
 import org.droolsfiddle.rest.*;
 import org.droolsfiddle.rest.model.*;
-import org.droolsfiddle.rest.model.PackageDTO;
 import org.droolsfiddle.websocket.CustomDroolsEvent;
 import org.droolsfiddle.websocket.WebSocketUtil;
 import org.jboss.resteasy.logging.Logger;
