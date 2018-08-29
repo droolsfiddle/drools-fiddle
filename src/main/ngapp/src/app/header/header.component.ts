8ed144730236c57ec50eaafb6137b73bf40c4133
Alexis OLIVARI <Alexis.OLIVARI@amadeus.com>
2018-08-29 18:43:05 +0200

    Fix websocket bug

--- src/main/ngapp/src/app/header/header.component.ts
+++ src/main/ngapp/src/app/header/header.component.ts
@@ -3,6 +3,8 @@ import {DRLService} from '../services/drl.service';
 import {EventsService} from '../services/events.service';
 import {Subscription} from 'rxjs';
 import {StepFunctionsService} from "../services/step-functions.service";
+import {SocketService} from "../services/socket.service";
+
 
 
 /*  This component is the Header, it displays the navbar on the top.
@@ -29,7 +31,7 @@ export class HeaderComponent implements OnInit, OnDestroy {
     /* dataTarget: string; */
 
 
-    constructor(private drlService: DRLService, private eventService: EventsService, private stepFunctionService: StepFunctionsService) {
+    constructor(private drlService: DRLService, private eventService: EventsService, private stepFunctionService: StepFunctionsService, private socketService: SocketService) {
     }
 
 
